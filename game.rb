b33a3d7d9a85c10901de47ea56b518392eacc74a
EnzoMbz <89809584+EnzowMb@users.noreply.github.com>
2024-01-09 15:20:40 -0300

    feat: learning if else

--- game.rb
+++ game.rb
@@ -4,3 +4,13 @@ puts 'Qual o seu nome?'
 
 puts
 puts "Olá #{@name}Começaremos o jogo para você!"
+puts "Escolha um numero entre 1 a 200"
+@numeroSorteado = 143
+puts "Tentativa 1"
+@chute = gets
+puts "Você chutou #{@chute}"
+if @chute.to_i == @numeroSorteado
+    puts "Acertou"
+else
+    puts "Errou"
+end

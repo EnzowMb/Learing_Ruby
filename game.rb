fb29f660c5d136a3d5bcb57aab80d884b9f3d2fc
EnzoMbz <89809584+EnzowMb@users.noreply.github.com>
2024-01-10 19:16:57 -0300

    feat: init with function

--- game.rb
+++ game.rb
@@ -1,11 +1,18 @@
-puts 'Bem vindo ao jogo de adivinhação!'
-puts 'Qual o seu nome?'
-@name = gets
+def boasVindas
+    puts 'Bem vindo ao jogo de adivinhação!'
+    puts 'Qual o seu nome?'
+    @name = gets
+end
 
-puts
-puts "Olá #{@name}Começaremos o jogo para você!"
-puts "Escolha um numero entre 1 a 200"
-@numeroSorteado = 143
+def definirNumero
+    puts "Olá #{@name}Começaremos o jogo para você!"
+    puts "Escolha um numero entre 1 a 200"
+    return numeroSorteado = 143
+end
+
+boasVindas
+definirNumero
+sorteado = definirNumero
 
 @limiteDeTentativas = 5
 for @tentativa in 1..@limiteDeTentativas
@@ -15,12 +22,12 @@ puts "Você chutou #{@chute}"
 
 
 
-@acertou = @chute.to_i == @numeroSorteado
+@acertou = @chute.to_i == sorteado
 if @acertou
     puts "Acertou"
     break
 else
-    @maior = @numeroSorteado > @chute.to_i
+    @maior = sorteado > @chute.to_i
         if @maior
             puts 'Numero é maior'
         else

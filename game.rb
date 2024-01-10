03f8b8fbf30f56a0c484e32f8ffed41de439eb11
EnzoMbz <89809584+EnzowMb@users.noreply.github.com>
2024-01-10 19:48:26 -0300

    feat: learning break

--- game.rb
+++ game.rb
@@ -10,28 +10,35 @@ def definirNumero
     return numeroSorteado = 143
 end
 
-boasVindas
-definirNumero
-sorteado = definirNumero
+def pedeNumero(tentativa, limiteDeTentativas)
+    puts "Tentativa " + tentativa.to_s + " de " + limiteDeTentativas.to_s
+    chute = gets
+    puts "Você chutou #{@chute}"
+    chute.to_i #não precisa por return se é na ultima linha!
+end
 
-@limiteDeTentativas = 5
-for @tentativa in 1..@limiteDeTentativas
-puts "Tentativa #{@tentativa} de #{@limiteDeTentativas}"
-@chute = gets
-puts "Você chutou #{@chute}"
-
-
-
-@acertou = @chute.to_i == sorteado
-if @acertou
-    puts "Acertou"
-    break
-else
-    @maior = sorteado > @chute.to_i
+def verificaAcerto(chute, sorteado)
+    @acertou = chute == sorteado
+    if @acertou
+        puts "Acertou"
+        return true
+    end
+    @maior = sorteado > chute
         if @maior
             puts 'Numero é maior'
         else
             puts "Numero é menor"
         end
+    false
 end
+
+boasVindas
+definirNumero
+sorteado = definirNumero
+limiteDeTentativas = 5
+
+for tentativa in 1..limiteDeTentativas
+chute = pedeNumero(tentativa, limiteDeTentativas)
+break if verificaAcerto(chute, sorteado)
+
 end

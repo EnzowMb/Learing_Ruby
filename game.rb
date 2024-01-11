4262586f07add7d8beb92d91276e400f9f637fc9
EnzoMbz <89809584+EnzowMb@users.noreply.github.com>
2024-01-11 15:47:33 -0300

    feat: learning strip "enter"

--- game.rb
+++ game.rb
@@ -1,11 +1,11 @@
 def boasVindas
     puts 'Bem vindo ao jogo de adivinhação!'
     puts 'Qual o seu nome?'
-    @name = gets
+    @name = gets.strip
 end
 
 def definirNumero
-    puts "Olá #{@name}Começaremos o jogo para você!\n\n"
+    puts "Olá #{@name} começaremos o jogo para você!\n\n"
     puts "Escolha um numero entre 1 a 200"
     return numeroSorteado = 143
 end
@@ -13,7 +13,7 @@ end
 def pedeNumero(chutes, tentativa, limiteDeTentativas)
     puts "\nTentativa " + tentativa.to_s + " de " + limiteDeTentativas.to_s
     puts "Chutes até agora: " + chutes.to_s
-    chute = gets
+    chute = gets.strip
     puts "Você chutou " + chute
     chute.to_i #não precisa por return se é na ultima linha!
 end

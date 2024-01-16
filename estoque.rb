138c812cc968e4735793080efdab16f8fbb3ac26
EnzoMbz <89809584+EnzowMb@users.noreply.github.com>
2024-01-16 12:48:51 -0300

    feat: OO with ruby

--- estoque.rb
+++ estoque.rb
@@ -43,35 +43,29 @@ class Estoque
         @livros.maximo_necessario
     end
 
-    def quantidade_de_vendas_por(produto, &campo)
-        @vendas.count { |venda| campo.call(venda) == campo.call(produto) }
+    def method_missing(name)
+        matcher = name.to_s.match("(.+)_que_mais_vendeu_por_(.+)")  #(.+) -> significa "qualquer coisa", entao seria qualquer coisa entre _que_mais_vendeu_por_
+  
+        if matcher
+           tipo = matcher[1]
+           campo = matcher[2].to_sym  #pois precisamos converter para simbolo
+           que_mais_vendeu_por(tipo, &campo)
+        else
+           super
+        end 
     end
 
-    def que_mais_vendeu_por(tipo, &campo)
-        @vendas.select { | produto | produto.tipo == tipo}.sort {|v1,v2| quantidade_de_vendas_por(v1, &campo) <=> quantidade_de_vendas_por(v2, &campo)}.last
+    #verifique se o método que estamos chamando é equivalente ao que esperamos, ou se nossa classe pai (super) responde por este método
+    def respond_to?(name)
+        name.to_s.match ("(.+)_que_mais_vendeu_por_(.+)") || super
     end
 
-    def livro_que_mais_vendeu_por_titulo
-        que_mais_vendeu_por("livro", &:titulo)
-    end
-  
-    def livro_que_mais_vendeu_por_ano
-        que_mais_vendeu_por("livro", &:ano_lancamento)
-    end
-  
-    def livro_que_mais_vendeu_por_editora
-        que_mais_vendeu_por("livro", &:editora)
-    end
-  
-    def revista_que_mais_vendeu_por_titulo
-        que_mais_vendeu_por("revista", &:titulo)
-    end
-  
-    def revista_que_mais_vendeu_por_ano
-        que_mais_vendeu_por("revista", &:ano_lancamento)
-    end
-  
-    def revista_que_mais_vendeu_por_editora
-        que_mais_vendeu_por("revista", &:editora)
-    end
+    private
+        def quantidade_de_vendas_por(produto, &campo)
+            @vendas.count { |venda| campo.call(venda) == campo.call(produto) }
+        end
+
+        def que_mais_vendeu_por(tipo, &campo)
+            @vendas.select{ | l |  l.tipo == tipo}.sort {|v1,v2| quantidade_de_vendas_por(v1, &campo) <=> quantidade_de_vendas_por(v2, &campo)}.last
+        end
 end
--- sistema.rb
+++ sistema.rb
@@ -5,18 +5,30 @@ estoque = Estoque.new
 algoritmos = Livro.new("Opa", 10, 2021, true, "editora", "livro")
 arquitetura = Livro.new("Opa2", 10, 2021, true, "editora", "livro")
 programmer = Livro.new("Programmer", 10, 2020, true, "editora", "livro")
-estoque <<  algoritmos
-puts estoque.maximo_necessario
-estoque <<  arquitetura
-puts estoque.maximo_necessario
-estoque <<  programmer
-puts estoque.maximo_necessario
+
+estoque <<  algoritmos <<  arquitetura <<  programmer <<  programmer
 
 estoque.vende(algoritmos)
 estoque.vende(programmer)
 estoque.vende(programmer)
 
-puts estoque.livro_que_mais_vendeu_por_titulo.titulo
+
 revistona = Livro.new("Revista de Ruby", 10, 2012, true, "Revistas", "revista")
+
+estoque << revistona
+puts estoque.total
 estoque.vende(revistona)
-puts estoque.revista_que_mais_vendeu_por_titulo.titulo
+
+puts estoque.livro_que_mais_vendeu_por_titulo(&:titulo).titulo
+puts estoque.revista_que_mais_vendeu_por_titulo(&:titulo).titulo
+
+online_arquitetura = Livro.new("Introdução a Arquitetura e Design de Software", 50, 2012, true, "editora", "ebook")
+
+estoque << online_arquitetura
+puts estoque.total
+
+estoque.vende(online_arquitetura)
+
+puts estoque.ebook_que_mais_vendeu_por_titulo(&:titulo).titulo
+
+puts estoque.respond_to?(:ebook_que_mais_vendeu_por_titulo)
